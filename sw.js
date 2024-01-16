b988034a59f55e3029e250afc873224239eec38b
jesusAG <33084455+jesusAG28@users.noreply.github.com>
2024-01-16 11:51:40 +0100

    \^o^/

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
-// Nombre del caché
-// if CACHE_NAME != version.json->version = auto update app
-const CACHE_NAME = 'v1.6';
-
 // Obtener el service worker actual
 // const sw = navigator.serviceWorker.controller;
 const sw = self;
+
+// Variable para controlar el estado de la actualización
+let updatingCache = false;
+
+let CACHE_NAME = 'v1.0';
 
 // Archivos para almacenar en caché
 const urlsToCache = [
@@ -13,15 +14,30 @@ const urlsToCache = [
     '/assets/app.js'
 ];
 
-
 // Evento de instalación
 self.addEventListener('install', event => {
-    self.skipWaiting();
     event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            console.log('Caché abierto');
-            return cache.addAll(urlsToCache);
+        caches.open('meta-data').then(cache => {
+            return cache.match('CACHE_NAME').then(response => {
+                if (response) {
+                    return response.text();
+                }
+                return null;
+            });
         })
+            .then(savedCacheName => {
+                const initialCacheName = savedCacheName || CACHE_NAME;
+                console.log('Loaded with cache:', initialCacheName);
+
+                // Almacenar los archivos en caché durante la instalación
+                return caches.open(initialCacheName).then(cache => {
+                    return cache.addAll(urlsToCache);
+                });
+            })
+            .then(() => {
+                // Activar el service worker inmediatamente después de la instalación
+                return self.skipWaiting();
+            })
     );
 });
 
@@ -29,6 +45,7 @@ self.addEventListener('install', event => {
 self.addEventListener('activate', event => {
     self.clients.claim();
     event.waitUntil(
+        // Realizar tareas de activación, como eliminar cachés antiguas
         caches.keys().then(cacheNames => {
             return Promise.all(
                 cacheNames.filter(cacheName => {
@@ -38,6 +55,9 @@ self.addEventListener('activate', event => {
                 })
             );
         })
+            // Escribir el nuevo valor en la caché 'meta-data'
+            .then(() => caches.open('meta-data'))
+            .then(cache => cache.put('CACHE_NAME', CACHE_NAME))
     );
 });
 
@@ -54,90 +74,85 @@ self.addEventListener('fetch', event => {
 });
 
 function checkForUpdate() {
-    fetch('/version.json') // Cambia la ruta según tu estructura de servidor
-        .then(response => response.json())
-        .then(data => {
-            const serverVersion = data.version;
-            console.log('Versión del servidor:', serverVersion);
-            console.log('Versión de cahce:', CACHE_NAME);
+    // Verificar si ya se está actualizando el caché
+    if (updatingCache) {
+        console.log('La actualización del caché está en curso. Esperando...');
+        return;
+    }
 
-            // Comprobar si hay una nueva versión
-            if (serverVersion != CACHE_NAME) {
-                console.log('Nueva versión encontrada. Actualizando...');
+    updatingCache = true;
 
-                // Descargar y activar la nueva versión
-                caches.open(serverVersion)
-                    .then(cache => {
-                        return cache.addAll(urlsToCache);
-                    })
-                    .then(() => {
-                        // Actualizar la versión actual del cache
-                        CACHE_NAME = serverVersion;
-                        console.log('Service worker actualizado a la versión', serverVersion);
-                    });
-            }
-        })
-        .catch(error => {
-            console.error('Error al comprobar la versión del servidor:', error);
-        });
+    // Obtener la versión actual del caché
+    let currentCacheName;
+
+    caches.open('meta-data').then(metaCache => {
+        return metaCache.match('CACHE_NAME');
+    }).then(response => {
+        if (response) {
+            return response.text();
+        }
+        return null;
+    }).then(savedCacheName => {
+        currentCacheName = savedCacheName || 'v1.0';
+        console.log('Versión de caché:', currentCacheName);
+
+        // Fetch de la versión del servidor
+        fetch('https://pwa.test/version.json', { cache: 'no-store' })
+            .then(response => response.json())
+            .then(data => {
+                const serverVersion = data.version;
+                console.log('Versión del servidor:', serverVersion);
+
+                // Comprobar si hay una nueva versión
+                if (serverVersion > currentCacheName) {
+                    console.log('Nueva versión encontrada. Actualizando...');
+
+                    // Descargar y activar la nueva versión
+                    caches.open(serverVersion)
+                        .then(cache => {
+                            return cache.addAll(urlsToCache);
+                        })
+                        .then(() => {
+                            // Abrir la caché 'meta-data' y actualizar el valor
+                            return caches.open('meta-data').then(metaCache => {
+                                // Utilizar 'put' con una nueva Response para almacenar el valor
+                                return metaCache.put('CACHE_NAME', new Response(serverVersion));
+                            });
+                        })
+                        .then(() => {
+                            console.log('Service worker actualizado a la versión', serverVersion);
+                        })
+                        .catch(error => {
+                            console.error('Error al actualizar el service worker:', error);
+                        })
+                        .finally(() => {
+                            updatingCache = false; // Marcar que la actualización ha terminado
+                        });
+                } else {
+                    updatingCache = false; // Marcar que la actualización ha terminado
+                }
+            })
+            .catch(error => {
+                console.error('Error al comprobar la versión del servidor:', error);
+                updatingCache = false; // Marcar que la actualización ha terminado en caso de error
+            });
+    });
+}
+
+function showLoader() {
+    const loaderContainer = document.getElementById('loader-container');
+    if (loaderContainer) {
+        loaderContainer.style.display = 'block';
+    }
+}
+
+function hideLoader() {
+    const loaderContainer = document.getElementById('loader-container');
+    if (loaderContainer) {
+        loaderContainer.style.display = 'none';
+    }
 }
 
 // Verificar la actualización cada 24 horas (ajusta según tus necesidades)
-setInterval(checkForUpdate, 5 * 1000);
-// setInterval(checkForUpdate, 24 * 60 * 60 * 1000);
-
-/*
-// Añadir un listener al evento updatefound
-self.addEventListener('updatefound', () => {
-    // Obtener el nuevo service worker
-    const newSW = sw.installing;
-
-    // Mostrar el loader
-    document.querySelector('.loader-container').style.display = 'block';
-
-    // Añadir un listener al evento statechange
-    newSW.addEventListener('statechange', () => {
-        // Comprobar el estado del nuevo service worker
-        switch (newSW.state) {
-            case 'installed':
-                // El nuevo service worker está instalado, pero no activado
-                // Podemos mostrar un mensaje para que el usuario recargue la página
-                document.querySelector('.text').textContent = 'Nueva versión disponible. Recarga la página para actualizar.';
-                break;
-            case 'activated':
-                // El nuevo service worker está activado
-                // Podemos ocultar el loader
-                document.querySelector('.loader-container').style.display = 'none';
-                break;
-        }
-    });
-});
-
-// Comprobar si hay una nueva versión del service worker
-sw.registration.update()
-    .then(() => {
-        // Si hay una nueva versión, se instalará y activará
-        console.log('Service worker actualizado');
-    })
-    .catch(error => {
-        // Si hay algún error, se mostrará en la consola
-        console.error('Error al actualizar el service worker', error);
-    });
-
-
-// Añadir un listener personalizado para verificar actualizaciones
-self.addEventListener('message', event => {
-    if (event.data && event.data.type === 'checkForUpdate') {
-        // Comprobar si hay una nueva versión del service worker
-        self.registration.update()
-            .then(() => {
-                // Si hay una nueva versión, se instalará y activará
-                console.log('Service worker actualizado');
-            })
-            .catch(error => {
-                // Si hay algún error, se mostrará en la consola
-                console.error('Error al actualizar el service worker', error);
-            });
-    }
-});
-*/
+setInterval(checkForUpdate, 5 * 1000); // 30s
+// setInterval(checkForUpdate, 24 * 60 * 60 * 1000); // 24H
